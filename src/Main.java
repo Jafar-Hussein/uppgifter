ecddadfe87b7eb8c2ed6f6b148d9237bb8095c0a
Jafar-Hussein <Jafarhussein6@gmail.com>
2023-02-24 15:38:49 +0100

    commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> namn=new ArrayList<String>();
+        namn.add("Luke");
+        namn.add("Leia");
+        namn.add("Han");
+
+        ArrayList<Integer> heltal = new ArrayList<Integer>();
+        heltal.add(421);
+        heltal.add(1977);
+        heltal.add(1138);
+        heltal.add(2187);
+
+        for (int i = 0; i<namn.size(); i++){
+            System.out.println(namn.get(i));
+
+        }
+        System.out.println();
+
+        for (int i = 0; i<heltal.size(); i++){
+            System.out.println(heltal.get(i));
+
+        }
+    }
+}
